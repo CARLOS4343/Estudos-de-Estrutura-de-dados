c80ec99dc34d7663444234496373a2482663fee1
CARLOS4343 <155836038+CARLOS4343@users.noreply.github.com>
2025-06-30 20:11:13 -0300

    Add files via upload
    Estudos de Arvore ABP

---
+++ ArvoresABP/src/Arborizavel.java
@@ -0,0 +1,14 @@
+
+
+    public interface Arborizavel<T> {
+
+        NoTriplo<T> getRaiz();
+        void inserir(T dado);
+        T apagar(T dado);
+        boolean existe(T dado);
+        void limpar();
+        String imprimirPreOrdem();
+        String imprimirEmOrdem();
+        String imprimirPosOrdem();
+    }
+
