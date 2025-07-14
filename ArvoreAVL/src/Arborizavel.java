558360a4e37bd5062f74ddea7ed56308d2d23ec7
CARLOS4343 <155836038+CARLOS4343@users.noreply.github.com>
2025-07-14 19:28:56 -0300

    Aula de árvore AVL

---
+++ ArvoreAVL/src/Arborizavel.java
@@ -0,0 +1,13 @@
+public class Arborizavel {
+    public interface Arborizavel<T> {
+
+        NoTriplo<T> getRaiz();
+        void inserir(T dado);
+        T apagar(T dado);
+        boolean existe(T dado);
+        void limpar();
+        String imprimirPreOrdem();
+        String imprimirEmOrdem();
+        String imprimirPosOrdem();
+    }
+}
